67f51dd1bc98bc04c5f94dab3f7b8c7b99468139
StefanoM98 <smartino030@gmail.com>
2023-03-15 14:09:36 +0100

    Add: work done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // CREIAMO LA LISTA DELLA SPESA QUINDI UN ARRAY
 const listItem = ["pane", "pasta", "latte", "mozzarella", "carciofi", "banane"];
-console.log(listItem)
+const items = document.querySelector("ul")
+
+let i = 0;
+
+while (i < listItem.length) {
+    items.innerHTML += `<li>${listItem[i]}</li>`
+    i++
+}
